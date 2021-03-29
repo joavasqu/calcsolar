f0a3831ee86d5b13ae893da1e7e76e1f0d1b50e3
joavasqu <joaquin.vasquez.elias@gmail.com>
2021-03-29 19:04:26 -0300

    Se agrega un index para las dos páginas que se están creando y se avanza bastante en el cálculo de consumo. Se logra hace r que una opción aparezca o desaparezca dependiendo del resultado de una anterior

---
+++ consumo.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+
+
+document.getElementById('tvNo').addEventListener('change', function(){
+    document.getElementById('cantTvLabel').style.display = 'none';
+    document.getElementById('cantTv').style.display = 'none' ;
+}
+)
+document.getElementById('tvSi').addEventListener('change', function(){
+    document.getElementById('cantTvLabel').style.display = 'block';
+    document.getElementById('cantTv').style.display = 'inline' ;
+}
+)
+
+
+
+ }, false)
+
+
+
